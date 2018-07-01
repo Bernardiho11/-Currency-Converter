aaca8df45c3f33e2f288235934035302ff07449f
Bernard O <bernardiho4personnal@gmail.com>
2018-07-01 17:27:01 +0100

    Minor changes

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ self.addEventListener('install', (event)=>{
   );
 })
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', (event)=> {
   event.respondWith(
       caches.match(event.request).then((response)=>{
         if (response) return response;
